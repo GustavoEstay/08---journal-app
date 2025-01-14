bcb6b56b969ea8be2da9a398b1bd549afc8e752c
Gustavo <cgustavoestay@gmail.com>
2025-01-13 21:21:13 -0300

    NothingSelectedView implementation

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,14 @@
 import { Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout';
+import NothingSelectedView from '../view/NothingSelectedView';
 
 export const JournalPage = () => {
   return (
     <>    
     <JournalLayout>
-      <Typography>
-        Holis
-      </Typography>
+
+      <NothingSelectedView/>
 
       {/* NothingSelected  */}
 
