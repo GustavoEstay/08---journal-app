2f013ae4e51fdd7dbdb51e61cfcdf59af9dad317
Gustavo <cgustavoestay@gmail.com>
2025-01-13 22:17:54 -0300

    IconButton flotante implementation

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,31 +1,32 @@
 
-import { Box } from '@mui/system'
-import React from 'react'
-import { NavBar, SideBar } from '../components';
 import { Toolbar } from '@mui/material';
+import { Box } from '@mui/system'
+import { NavBar, SideBar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
-    <Box sx={{display:'flex'}}>
+    <Box sx={{ display: 'flex' }}>
 
-        {/* NavBar */}
-        <NavBar drawerWidth={ drawerWidth }/>
+      {/* NavBar */}
+      <NavBar drawerWidth={drawerWidth} />
 
-        {/* SideBar */}
-        <SideBar drawerWidth={drawerWidth}/>
+      {/* SideBar */}
+      <SideBar drawerWidth={drawerWidth} />
 
-        <Box component='main'
-        sx={{flexGrow: 1, p: 3
+      <Box
+        component='main'
+        sx={{
+          flexGrow: 1, p: 3
         }}
-        >
+      >
 
-            {/* toolbar */}
-            <Toolbar/>
+        {/* toolbar */}
+        <Toolbar />
 
-            { children }
-        </Box>
+        {children}
+      </Box>
 
     </Box>
   )
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,6 +1,6 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
-import ImageGallery from "../components/ImageGallery"
+import { ImageGallery } from "../components"
 
 const NoteView = () => {
     return (
